6ab7c42086ff1e63eeff5431b1238338144be1f8
Willys <willysvarela@gmail.com>
2020-04-19 19:17:31 -0400

    feat: added libs to init work

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const HomeScreen = () => {
+  return <View />;
+};
+
+export default HomeScreen;
